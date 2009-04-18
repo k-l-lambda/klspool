68229c186a70105358ce648368e7a6b36654edb5
xxxK.L.xxx <xxxK.L.xxx@64c72148-2b1e-11de-874f-bb9810f3bc79>
2009-04-18 11:33:14 +0000

    game framework based on OGRE set up.

---
+++ trunk/Pool/Game/PrecompiledHeaders.cpp
@@ -0,0 +1,15 @@
+/*
+**	This source file is part of Pool.
+**
+**	Copyright (c) 2009	K.L.<xxxk.l.xxx@gmail.com>, Lazy<yinxiaozhou@gmail.com>
+**	This program is free software without any warranty.
+*/
+
+// PrecompiledHeaders.cpp : 只包括标准包含文件的源文件
+// Pool.pch 将成为预编译头
+// PrecompiledHeaders.obj 将包含预编译类型信息
+
+#include "StableHeaders.h"
+
+// TODO: 在 StableHeaders.h 中
+//引用任何所需的附加头文件，而不是在此文件中引用
