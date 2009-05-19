22c7c9d5ecc19e9f09e88d3307b1f80c2d6e10a9
xxxK.L.xxx <xxxK.L.xxx@64c72148-2b1e-11de-874f-bb9810f3bc79>
2009-05-19 03:22:22 +0000

    WorkPath\DeployStables.bat: added. WorkPath\DeployVariables.bat: added.

---
+++ trunk/Pool/WorkPath/DeployVariables.bat
@@ -0,0 +1,10 @@
+
+copy	..\bin\release\PoolGame.exe							%1\
+copy	..\bin\release\AudioSystem.dll						%1\
+copy	..\bin\release\Billiards.dll						%1\
+copy	ReadMe.txt											%1\
+copy	BallConfigSets\*.xml								%1\BallConfigSets\
+copy	Hosts\*.lua											%1\Hosts\
+copy	Media\gui\Pool.layout								%1\Media\gui\
+copy	Media\materials\scripts\Pool.material				%1\Media\materials\scripts\
+copy	Media\materials\scripts\PoolTable.material			%1\Media\materials\scripts\
